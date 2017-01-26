d60e4ea1a4bfc1494c152c015255b017082bfcc3
Eric Savoie <ericsavoie@BSCMACLT0318.local>
2017-01-26 00:04:20 -0400

    delete function

--- lib/shared_functions.rb
+++ lib/shared_functions.rb
@@ -7,17 +7,4 @@ module SharedFunctions
       end
     end
   end
-
-  def useful_initialize_error
-      instance_variables.each do |v|
-        begin
-          define_singleton_method(v.to_s.tr('@','')) do
-            instance_variable_get(v)
-          end
-        rescue
-          "Can't find #{v} on page"
-        end
-      end
-  end
-
 end
